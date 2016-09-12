8b1fbc2b82a71dd56dc14071d6f6404b36aeaeb0
sammysam5909 <ashbys@wit.edu>
2016-09-12 14:26:48 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double i = 50.0;
+		double k = i + 50.0;
+		double j = k + 1;
+		
+		System.out.println("j is " + j + " and k is " + k);
+		
+
+	}
+
+}
---
+++ Variables.java
@@ -0,0 +1,16 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 1.0;
+		double area = radius * radius * 3.14159;
+		System.out.println("The area is " + area + " for radius " + radius);
+		
+		double radius2 = 2.0;
+		double area2 = radius2 * radius2 * 3.14159;
+		System.out.println("The area is " + area2 + " for radius " + radius2);
+
+	}
+
+}
